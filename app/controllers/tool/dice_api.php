2a146f5524ae1ea5cc1778220a8c65c8bcc04282
Maurick Starkvind <maverickk@gmail.com>
2026-09-03 21:39:37 +0200

    security: route tool API auth through request headers

--- app/controllers/tool/dice_api.php
+++ app/controllers/tool/dice_api.php
@@ -235,7 +235,7 @@ if (!hg_runtime_require_db($link, 'dice_api', 'plain', [
     return;
 }
 
-if (!hg_tool_api_require_token((string)($_GET['token'] ?? ''))) {
+if (!hg_tool_api_require_request_token()) {
     return;
 }
 
