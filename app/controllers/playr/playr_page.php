1fa455543340d9bc1a6db7d0f24381c23ac3d649
Maurick Starkvind <maverickk@gmail.com>
2026-09-04 13:28:09 +0200

    refactor: simplify player stylesheet registration

--- app/controllers/playr/playr_page.php
+++ app/controllers/playr/playr_page.php
@@ -58,11 +58,7 @@ include("app/partials/main_nav_bar.php");
 if (function_exists('hg_page_register_stylesheet')) {
     hg_page_register_stylesheet('/assets/css/hg-playr.css');
 } else {
-    if (function_exists('hg_page_register_stylesheet')) {
-        hg_page_register_stylesheet('/assets/css/hg-playr.css');
-    } else {
-        echo '<link rel="stylesheet" href="/assets/css/hg-playr.css">';
-    }
+    echo '<link rel="stylesheet" href="/assets/css/hg-playr.css">';
 }
 
 if (!function_exists('sanitize_int_csv')) {
