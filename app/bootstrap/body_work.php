330aa0a31565a7f638b5a312f2e10f013b751c09
Maurick Starkvind <maverickk@gmail.com>
2026-09-07 19:54:55 +0200

    fix: refresh request context after pretty normalization

--- app/bootstrap/body_work.php
+++ app/bootstrap/body_work.php
@@ -5,6 +5,7 @@ require __DIR__ . '/page_context.php';
 $routes = require __DIR__ . '/../routing/routes.php';
 
 normalize_pretty_request($link, $routeKey);
+$hgRequest = hg_request_context_from_query($_GET);
 require __DIR__ . '/../http/dispatcher.php';
 
 // Fallback de metatags desde títulos de página si no se han definido
