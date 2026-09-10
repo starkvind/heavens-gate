e042921362a63a8a34bfa216964a71a089243e8b
Maurick Starkvind <maverickk@gmail.com>
2026-09-10 12:23:21 +0200

    refactor: normalize desktop request without GET mutation

--- app/bootstrap/body_work.php
+++ app/bootstrap/body_work.php
@@ -1,11 +1,22 @@
 <?php
 
 require __DIR__ . '/page_context.php';
+require_once __DIR__ . '/../http/pretty_request.php';
 
+$routeKey = hg_request_route($hgRequest);
+$routeParam = hg_request_query_param($hgRequest, 't');
 $routes = require __DIR__ . '/../routing/routes.php';
 
-normalize_pretty_request($link, $routeKey);
-$hgRequest = hg_request_context_from_query($_GET);
+$hgQuery = hg_pretty_request_normalize(
+    $link,
+    $routeKey,
+    $hgQuery,
+    (string)($_SERVER['REQUEST_URI'] ?? '/')
+);
+$hgRequest = hg_request_context_from_query($hgQuery);
+$routeKey = hg_request_route($hgRequest);
+$routeParam = hg_request_query_param($hgRequest, 't');
+
 require __DIR__ . '/../http/dispatcher.php';
 
 // Fallback de metatags desde títulos de página si no se han definido
