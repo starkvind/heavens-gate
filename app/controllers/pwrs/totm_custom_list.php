95f3bbc366492d66f0034fbb9dd39fc83c152786
Maurick Starkvind <maverickk@gmail.com>
2026-09-10 15:22:38 +0200

    refactor: pass request context to custom totems

--- app/controllers/pwrs/totm_custom_list.php
+++ app/controllers/pwrs/totm_custom_list.php
@@ -2,4 +2,4 @@
 require_once __DIR__ . '/../../helpers/power_custom_pages.php';
 
 $config = hg_power_custom_catalog_totems($link);
-hg_power_custom_render($link, $config);
+hg_power_custom_render($link, $config, $hgRequest);
