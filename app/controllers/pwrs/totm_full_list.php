22e57be5b41a1f33ca7d913f57e93743c6d8d057
Maurick Starkvind <maverickk@gmail.com>
2026-09-10 15:22:52 +0200

    refactor: pass request context to full totems

--- app/controllers/pwrs/totm_full_list.php
+++ app/controllers/pwrs/totm_full_list.php
@@ -4,4 +4,4 @@ require_once __DIR__ . '/../../helpers/power_custom_pages.php';
 $config = hg_power_custom_catalog_totems($link);
 $config['meta_description'] = 'Listado completo de tótems en formato extendido.';
 $config['intro'] = 'Listado completo de tótems, con acceso rápido y ficha completa.';
-hg_power_custom_render_full_catalog($link, $config);
+hg_power_custom_render_full_catalog($link, $config, $hgRequest);
