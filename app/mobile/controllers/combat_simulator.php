d6b0928c9664c2ab5646db172087d5ab3aa2c103
Maurick Starkvind <maverickk@gmail.com>
2026-09-24 23:58:14 +0200

    refactor: archive retired game runtime

--- app/mobile/controllers/combat_simulator.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-http_response_code(410);
-?>
-<section class="hg-mobile-section">
-    <h1>Simulador de Combate retirado</h1>
-    <p>Esta herramienta ya no forma parte de Heaven&apos;s Gate.</p>
-</section>
