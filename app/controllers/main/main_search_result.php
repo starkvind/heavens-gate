9320d456eeecb2ef03d8bd93192dfb83ca93841f
Maurick Starkvind <maverickk@gmail.com>
2026-09-25 20:02:29 +0200

    refactor: resolve inventory search with canonical route

--- app/controllers/main/main_search_result.php
+++ app/controllers/main/main_search_result.php
@@ -114,7 +114,7 @@ function hg_search_result_url(mysqli $link, string $routeKey, int $id): string
         case 'temp': return pretty_url($link, 'dim_seasons', '/seasons', $id);
         case 'seechapter': return pretty_url($link, 'dim_chapters', '/chapters', $id);
         case 'verdoc': return pretty_url($link, 'fact_docs', '/documents', $id);
-        case 'seeitem': return hg_search_item_url($link, $id);
+        case 'verobj': return hg_search_item_url($link, $id);
         case 'muestradon': return pretty_url($link, 'fact_gifts', '/powers/gift', $id);
         case 'verrasgo': return pretty_url($link, 'dim_traits', '/rules/traits', $id);
         case 'sistemas': return pretty_url($link, 'dim_systems', '/systems', $id);
