87e046211b83ea6b8086f50c34be74e7f7f022ba
Maurick Starkvind <maverickk@gmail.com>
2026-09-26 09:36:15 +0200

    fix: load shared chronicle scope from domain

--- app/mobile/controllers/documents.php
+++ app/mobile/controllers/documents.php
@@ -2,7 +2,7 @@
 
 include_once(__DIR__ . '/../../helpers/public_response.php');
 include_once(__DIR__ . '/../../helpers/character_avatar.php');
-include_once(__DIR__ . '/../helpers/chronicle_scope.php');
+require_once(__DIR__ . '/../../domains/chronicles/scope.php');
 require_once(__DIR__ . '/../../domains/documents/queries.php');
 
 $metaTitle = "Documentos | Heaven's Gate";
